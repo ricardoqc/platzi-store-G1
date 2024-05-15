81c6bf56b9b416ffe8939bbf84eed78cf35cdb9d
ErickRabago <164416841+ErickRabago@users.noreply.github.com>
2024-05-15 12:28:05 -0700

    añadi el formulario, html y css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import react from 'react';
+import { Clase2 } from './components/Clase2';
+import { Clase4 } from './components/Clase4';
 
 function App() {
 
   return (
     <>
-      <h1>Platzi Store - G1</h1>
     </>
   )
 }
